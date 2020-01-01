1d9866fdbb66b5effd7f9f1aec5930c6b4890e3b
Joakim Johansson <joakim.johansson@allbinary.se>
2020-01-01 23:27:02 +0100

    Started adding numpad input.

---
+++ Assets/Scripts/NumberInput.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberInput : MonoBehaviour {
+    public int max = 99;
+    public int min = 1;
+    public Button okBtn;
+    public Button eraseBtn;
+    public TextMeshProUGUI text;
+    private string display = "1";
+    private int value = 1;
+
+    void Start() {
+        text.SetText(display);
+        // okBtn.onClick.AddListener()
+        eraseBtn.onClick.AddListener(Erase);
+    }
+
+    public void Add(string digit) {
+        int new_val;
+        if (int.TryParse(display += digit, out new_val)) {
+            display += digit;
+            UpdateUI(new_val);
+            value = new_val;
+        }
+    }
+
+    public void Erase() {
+        //Remove last digit from string.
+        if (display.Length > 1) {
+            display = display.Substring(0, display.Length - 1);
+            int new_val;
+            if (int.TryParse(display, out new_val)) {
+                UpdateUI(new_val);
+                value = new_val;
+            } else {
+                UpdateUI();
+            }
+        }
+    }
+
+    private void UpdateUI(int val = int.MinValue) {
+        if (display.Length > 0) {
+            eraseBtn.interactable = true;
+        } else {
+            eraseBtn.interactable = false;
+        }
+
+        if (val <= max && val >= min) {
+            okBtn.interactable = true;
+        } else {
+            okBtn.interactable = false;
+        }
+        text.SetText(display);
+    }
+}
---
+++ Assets/Scripts/NumberInputBtn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberInputBtn : MonoBehaviour {
+    private NumberInput parent;
+    private string digit;
+    private Button btn;
+    void Awake() {
+        parent = GetComponentInParent<NumberInput>();
+        digit = GetComponentInChildren<TextMeshProUGUI>().text;
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+    }
+
+    void OnDestroy() {
+        btn.onClick.RemoveAllListeners();
+    }
+
+    public void OnClick() {
+        if (parent && digit.Length > 0) {
+            parent.Add(digit);
+        } else {
+            throw new System.Exception("Uninitialized NumberInputButton.");
+        }
+    }
+}
